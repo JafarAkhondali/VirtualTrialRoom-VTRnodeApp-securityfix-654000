cb6732c367ab7914f6f2a104c82740eddf8932e5
Jmyeluri <jyelur2@illinois.edu>
2015-11-22 00:06:32 -0600

    port change

--- main.js
+++ main.js
@@ -4,8 +4,7 @@ var path    = require("path");
 
 
 app.use('/',express.static(__dirname + '/public'));
-app.use('/',express.static(__dirname + 'CSS'));
-app.listen(3000, function(){console.log('Listening at Port 3000')});
+app.listen(80, function(){console.log('Listening at Port 80')});
 
 
 
