10b94395aef012f92282a63558510ef2195c9f70
CallAnotherBarry <computermaniac9999@gmail.com>
2015-11-28 02:50:07 -0600

    Login complete commit

--- public/assets/js/checklogin.js
+++ public/assets/js/checklogin.js
@@ -7,11 +7,21 @@ function start(){
     var rootRef = new Firebase('https://vivid-fire-6481.firebaseio.com/');
     var auth=rootRef.getAuth();
     if(auth){
-        $('.logtext').text("Logout").attr("href","logout.html");
+        $('.logtext').text("Logout").attr("href","").on('click',unauthenticate);
     }else{
         $('.loginout').attr("href","login.html");
+        $('.logtext').text("Login").on('click',function(){});
     }
-
+    function unauthenticate(){
+        rootRef.unauth();
+        var loggedOut=(boolean)(rootRef.getAuth());
+        if(loggedOut){
+            $('.logtext').text("Login").on('click',function(){});
+            $('.loginout').attr("href","login.html");
+        }else{
+            alert("didn't work");
+        }
+    }
 
    
 
